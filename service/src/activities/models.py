e7cc1fe6a9d31c8d24b1506f10482f0704463974
Zeljko Kojic <kojicz983@gmail.com>
2018-10-19 21:19:47 +0200

    service: Add app models

---
+++ service/src/activities/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Location(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50, unique=True)
+
+class Topic(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50, unique=True)
+
+class SDG(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50, unique=True)
+
+class Category(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=50, unique=True)
+
+class Activities(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    sdg = models.ForeignKey(SDG, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    project_name = models.CharField(null=False, blank=False, max_length=50)
+    portfolio = models.CharField(null=False, blank=False, max_length=50)
+    cluster = models.CharField(null=False, blank=False, max_length=50)
+    specific_activity = models.CharField(null=False, blank=False, max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    donor_1 = models.CharField(null=False, blank=False, max_length=50)
+    donor_2 = models.CharField(null=False, blank=False, max_length=50)
+    donor_3 = models.CharField(null=False, blank=False, max_length=50)
+    activity_value = models.IntegerField()
+    beneficiaries = models.CharField(null=False, blank=False, max_length=50)
