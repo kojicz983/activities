a8385d56d7487b6803f3dabdcf757b2590c3b6b4
Zeljko Kojic <kojicz983@gmail.com>
2018-10-22 15:32:54 +0200

    Add all serializers

--- service/src/activities/views.py
+++ service/src/activities/views.py
@@ -1,12 +1,42 @@
 from rest_framework import serializers, viewsets
-from .models import Activities
+from .models import Activities, Location, Category, Topic, SDG
 
 
 # Create your views here.
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+
+class SDGSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SDG
+        fields = '__all__'
+
+
 class ActivitiesSerializer(serializers.ModelSerializer):
+    location = SDGSerializer()
+    category = CategorySerializer()
+    topic = TopicSerializer()
+    sdg = SDGSerializer()
+
     class Meta:
         model = Activities
         fields = '__all__'
+
 
 class ActivitiesViewSet(viewsets.ModelViewSet):
     queryset = Activities.objects.all()
